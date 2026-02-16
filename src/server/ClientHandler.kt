741b3a3bcf886d9aa91753bdea72fcb3fdffe5f4
Cook, Marius Werner <marius.cook@skatteetaten.no>
2026-02-16 10:25:15 +0100

    chatresponse runs on own thread so client and server can exchange messages continuously

--- src/server/ClientHandler.kt
+++ src/server/ClientHandler.kt
@@ -2,8 +2,10 @@ package server
 
 import java.io.BufferedReader
 import java.io.BufferedWriter
+import java.io.IOException
 import java.io.PrintWriter
 import java.net.Socket
+import kotlin.concurrent.thread
 
 
 class ClientHandler(clientSocket: Socket) {
@@ -11,31 +13,50 @@ class ClientHandler(clientSocket: Socket) {
     private val reader = BufferedReader(clientSocket.getInputStream().reader())
     private val writer = BufferedWriter(clientSocket.getOutputStream().writer())
     private var running: Boolean = false
+    private val user = clientSocket.localAddress
 
     fun handle() {
         running = true
         println("Someone has started a chat with you")
 
         //Sending message to client:
-        writeMessage("Hello, type something")
+        welcomeMessage("$user:  Welcome to the server, type something")
 
-        //socket connection is OK
+        //running client on seperate thread
+        thread {
+            while (running) {
+                try {
+                    readMessage()
+                } catch (e: Exception) {
+                    running = false
+                }
+            }
+        }
+
+        //running client on seperate thread
         while (running) {
             try {
-                //reading message
-                readMessage()
+                respondMessage()
             } catch (e: Exception) {
-                throw (IllegalArgumentException(e))
+                running = false
             }
         }
     }
+
+
     private fun readMessage() {
-        return println(reader.readLine())
-    }
-    private fun writeMessage(message: String) {
+        val message = reader.readLine()
+        println("The user wrote: $message")
+
+        }
+
+    private fun welcomeMessage(message: String) {
         return PrintWriter(writer, true)
             .println(message)
     }
+    private fun respondMessage() {
+        val input = readln()
+        PrintWriter(writer, true).println("Server responds with: $input")
+    }
+
 }
-
-
